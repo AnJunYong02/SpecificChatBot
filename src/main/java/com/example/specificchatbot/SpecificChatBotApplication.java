71d7c2004fd21204edd46eee7e17f1cd6ccc859e
JunYong An <happihanjy@naver.com>
2025-07-11 13:07:21 +0900

    Start project

---
+++ src/main/java/com/example/specificchatbot/SpecificChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.specificchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificChatBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpecificChatBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/specificchatbot/SpecificChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.specificchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecificChatBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
